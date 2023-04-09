28f20e1016b1b90b3fd59b8d65e95c8cf76135bf
XL <xl@lbx.dev>
2023-04-09 09:36:41 +0800

    Fix use latest/correct llama tokenizer

--- export_hf_checkpoint.py
+++ export_hf_checkpoint.py
@@ -3,16 +3,16 @@ import os
 import torch
 import transformers
 from peft import PeftModel
-from transformers import LlamaForCausalLM, LlamaTokenizer  # noqa: F402
+from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaForCausalLM  # noqa: F402
 
 BASE_MODEL = os.environ.get("BASE_MODEL", None)
 assert (
     BASE_MODEL
 ), "Please specify a value for BASE_MODEL environment variable, e.g. `export BASE_MODEL=decapoda-research/llama-7b-hf`"  # noqa: E501
 
-tokenizer = LlamaTokenizer.from_pretrained(BASE_MODEL)
+tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL, use_fast=True)
 
-base_model = LlamaForCausalLM.from_pretrained(
+base_model = AutoModelForCausalLM.from_pretrained(
     BASE_MODEL,
     load_in_8bit=False,
     torch_dtype=torch.float16,
@@ -53,5 +53,5 @@ deloreanized_sd = {
 }
 
 LlamaForCausalLM.save_pretrained(
-    base_model, "./hf_ckpt", state_dict=deloreanized_sd, max_shard_size="400MB"
+    base_model, "./hf_ckpt", state_dict=deloreanized_sd, max_shard_size="500MB"
 )
