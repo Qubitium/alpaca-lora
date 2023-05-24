1cbcb39daf5cc76053559f7a59a72197b4015a1b
diegomontoya <xing@fictionpress.com>
2023-05-24 06:42:56 +0000

    1) Control lora 2) 4bit 3) allow wandb resume

--- finetune.py
+++ finetune.py
@@ -244,11 +244,14 @@ def train(
             base_model,
             # max_memory=max_memory,
             load_in_4bit=True,
-            torch_dtype=torch.float16,  # bnb only supports float16
+            torch_dtype=torch.bfloat16,
             device_map=device_map,
+            # doc https://github.com/huggingface/transformers/pull/23479/files#diff-4333c40134efe7287ccda3bdd11e266b90a62629b933bf2cd15fa39cbf23b088R82
             quantization_config=BitsAndBytesConfig(
                 load_in_4bit=True,
                 bnb_4bit_quant_type="nf4",
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_compute_dtype=torch.bfloat16,
             ),
         )
 
